9781d92b641e891045f1f564cc5adf247d8c1dc5
AceTwin <cara.cornel@byu.edu>
2018-11-01 11:28:47 -0600

    Let's test it

--- Arduino/sonic/sonic.ino
+++ Arduino/sonic/sonic.ino
@@ -13,7 +13,29 @@
 const char* ssid = "ssid";       // WiFi name
 const char* password = "password"; // WiFi password
 
+//MQTT
+const char* mqtt_server = "x.x.x.x"; //Raspberry Pi's (MQTT Broker) IP
+const int mqttPort = 1883; //default port
+char* magnet_status; //Variable to hold whether door is closed or open?
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 int Status = 0; // This is the LED status we are going to send to the "distance" channel
+
+void callback(char* topic, byte* payload, unsigned int length) { //Watch MQTT messages
+ 
+  Serial.print("Message arrived in topic: "); //Which channel
+  Serial.println(topic);
+ 
+  Serial.print("Message:"); //prints the message from the channel
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+ 
+  Serial.println();
+  Serial.println("-----------------------"); //Seperates the messages
+ 
+}
 
 //variables for Sonic  
   
@@ -49,6 +71,29 @@ void setup()
       Serial.println(ssid);
       Serial.print("IP address: ");
       Serial.println(WiFi.localIP());
+
+    //MQTT Connection/Action
+    client.setServer(mqtt_server, mqttPort);
+    client.setCallback(callback);
+
+    //MQTT Connection or Fail to Connect
+    while (!client.connected()) {
+     Serial.print("Attempting MQTT connection...");
+     // Attempt to connect
+     if (client.connect("ESP8266Client")){ //If connected to MQTT Server
+      Serial.println("connected");
+     } 
+     else { //if not connected to MQTT Server
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+      }
+     }
+
+   //Subscribe to the distance channel 
+   client.subscribe("magnet"); //listen for messages in the magnet channel
 }  
 
 void loop() 
@@ -68,10 +113,16 @@ void loop()
     // Prints the distance on the Serial Monitor
     //Serial.print("Distance: ");
     //Serial.println(distance);
+
+    //converting int (the distance) to char -- Ref: https://www.instructables.com/id/Converting-integer-to-character/
+    char b[5];
+    String str=String(distance);
+    str.toCharArray(b,5);
     
     if (distance >= 100)
     {
       Status = 3;
+      client.publish("distance", b);
     }
     else if (distance >=50)
     {
@@ -85,5 +136,8 @@ void loop()
     {
       Status = 5;
     }
+
+    //code for MQTT
+    client.loop(); 
 
 }
